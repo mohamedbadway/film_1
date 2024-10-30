b6f1ce23ebed11bf84c9deb278b78145fad0e892
badway <badwaym19@gmail.com>
2024-10-30 17:09:41 +0300

    file html css

---
+++ slider-data.js
@@ -0,0 +1,27 @@
+let movies =[
+    {
+        name: ' WI vs IND 317/8(50)',
+des:'lorem ipsum dolor sit amet consectetur adipisicing elit.Velit porro et',           
+          image:'images/slider 2.PNG' 
+    },
+    {
+        name:'Rudra:The Edge of Darkness',
+        des:'lorem ipsum dolor sit amet  consectetur adipisicing elit. ',
+        image:'images/slider  1.PNG' 
+    },
+    {
+      name:'The Boook of Boba Fett  ',
+      des:'hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh',
+      image:'images/slider  3.PNG'
+    },
+    {
+        name:'raya and last dragon',
+        des: 'dddddddddddddddddddddddddddddd',
+        image: 'images/slider  4.PNG' 
+    },
+    {
+         name:'luca',
+         des:'dddddddddddddddddddddd',
+         image:'images/slider 5.PNG'
+    },
+ ]
